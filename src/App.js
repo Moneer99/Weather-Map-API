d1afac89ce959c291bc19dda9333e1dad1712d85
moneer99 <moneerbinoon9@gmail.com>
2022-12-31 21:30:34 +0300

    added style component and logic

--- src/App.js
+++ src/App.js
@@ -1,40 +1,51 @@
 import React, { useState } from "react";
 import "./App.css";
+import {
+  Container,
+  Main,
+  Search,
+  WeatherDisplay,
+  WeatherLocation,
+  WeatherDescription,
+  Temp,
+  TempRange,
+  TempRangeTxt,
+} from "./components/Styles";
 
 function App() {
-  function App() {
-    const [location, setLocation] = useState('');
-    const [weather, setWeather] = useState([]);
+  const [location, setLocation] = useState("");
+  const [weather, setWeather] = useState([]);
 
-  return(
+  return (
     <Container>
-    <Main> {typeof weather.main != 'undefined' ? (
-      <WeatherDisplay>
-        <WeatherLocation>
-          {weather.name} {weather.sys.country}
-        </WeatherLocation>
-        <WeatherDescription>
-          <span style={{ textTransform: 'capitalize', fontSize: '24px' }}>
-            {weather.weather[0].main}
-          </span>
-        </WeatherDescription>
-        <Temp>{weather.main.temp.toFixed(0)}°F</Temp>
-        <TempRange>
-          <TempRangeTxt>
-            H: {weather.main.temp_max.toFixed(0)}°
-          </TempRangeTxt>
-          <TempRangeTxt>
-            L: {weather.main.temp_min.toFixed(0)}°
-          </TempRangeTxt>
-        </TempRange>
-      </WeatherDisplay>
-    ) : (
-      ''
-    )}
-    <br />
-  </Main>
-  </Container>
-
+      <Main>
+        {" "}
+        {typeof weather.main != "undefined" ? (
+          <WeatherDisplay>
+            <WeatherLocation>
+              {weather.name} {weather.sys.country}
+            </WeatherLocation>
+            <WeatherDescription>
+              <span style={{ textTransform: "capitalize", fontSize: "24px" }}>
+                {weather.weather[0].main}
+              </span>
+            </WeatherDescription>
+            <Temp>{weather.main.temp.toFixed(0)}°F</Temp>
+            <TempRange>
+              <TempRangeTxt>
+                H: {weather.main.temp_max.toFixed(0)}°
+              </TempRangeTxt>
+              <TempRangeTxt>
+                L: {weather.main.temp_min.toFixed(0)}°
+              </TempRangeTxt>
+            </TempRange>
+          </WeatherDisplay>
+        ) : (
+          ""
+        )}
+        <br />
+      </Main>
+    </Container>
   );
 }
 
